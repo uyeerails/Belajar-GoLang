0b550893d8615ef24a4a745cbad20c09ab19952d
uyeerails <uyeerails@gmail.com>
2022-04-21 22:58:26 +0700

    Menambahkan Komentar Multiline

--- Belajar-Golang/project-pertama/main.go
+++ Belajar-Golang/project-pertama/main.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main() {
 	// Ini adalah Komentar
+	/*
+		Ini adalah Komentar Juga
+		Tapi Ini Multiline
+	*/
 	fmt.Println("Hello, Saya Dari Go-Lang!!!")
 	// Ini tidak akan dieksekusi
 }
