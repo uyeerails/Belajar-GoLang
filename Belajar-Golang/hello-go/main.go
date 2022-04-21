2b45acc17f0d13bfcea511b9bfdc58b50e35ed2c
uyeerails <uyeerails@gmail.com>
2022-04-21 23:25:40 +0700

    Perbaikan Komentar dan project variable-go

--- Belajar-Golang/hello-go/main.go
+++ Belajar-Golang/hello-go/main.go
@@ -1,13 +1,17 @@
+// Dalam sebuah project minimal harus ada sebuah package main
 package main
 
+// import digunakan untuk mengimport package, dalam hal ini fmt yanf fungsinya untuk keperluan I/O
 import "fmt"
 
+// Dalam Sebuah Project go harus ada file program dengan fungsi bernama main()
 func main() {
 	// Ini adalah Komentar
 	/*
 		Ini adalah Komentar Juga
 		Tapi Ini Multiline
 	*/
+	// Fungsi fmt.Println() untuk memunculkan text ke layar, berada dalam package fmt
 	fmt.Println("Hello, Saya Dari Go-Lang!!!")
 	// Ini tidak akan dieksekusi
 }
---
+++ Belajar-Golang/variable-go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Keyword var untuk deklarasi variable namaDepan diisi ketika deklarasi
+	var namaDepan string = "uyee"
+	// Keyword var untuk deklarasi variable namaBelakang diisi setelah baris kode deklarsi
+	var namaBelakang string
+	namaBelakang = "rails"
+
+	fmt.Printf("hai %s %s!\n", namaDepan, namaBelakang)
+}
