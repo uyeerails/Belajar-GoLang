f58497f78a310b13b1ffcdbea5498bc1c0f6ef8a
uyeerails <uyeerails@gmail.com>
2022-04-21 23:57:43 +0700

    Perbaikan variable-go

--- 1. GO-Basic/variable-go/main.go
+++ 1. GO-Basic/variable-go/main.go
@@ -7,10 +7,6 @@ func main() {
 	var namaDepan string = "uyee"
 	// Keyword var untuk deklarasi variable namaBelakang
 	var namaBelakang string = "rails"
-	// fmt.Printf() untuk menampilkan output dalam bentuk tertentu Cara 1
+	// fmt.Printf() untuk menampilkan output dalam bentuk tertentu
 	fmt.Printf("hai %s %s!\n", namaDepan, namaBelakang)
-	// fmt.Printf() menampilkan output dalam bentuk tertentu Cara 2
-	fmt.Printf("halo", namaDepan, namaBelakang+"!")
-	// fmt.Printf() menampilkan output dalam bentuk tertentu Cara 3
-	fmt.Printf("hai uyee rails!\n")
 }
